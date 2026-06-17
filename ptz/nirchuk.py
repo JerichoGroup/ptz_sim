2f1c1616fb42b684cf31edc886acb3b0f482465c
Mr-Blum <master420shifu420@tutamail.com>
2026-06-17 18:29:51 +0300

    With scenes at nirchuck

--- ptz/nirchuk.py
+++ ptz/nirchuk.py
@@ -5,10 +5,10 @@ from isaac_core_dev_kit.core_capture.video_capture import VideoCapture
 import isaac_core_dev_kit.dev_utils as core_utils
 import time
 
-CORE_PATH = "/home/ofer/clones/drones_gen"
-VID_LENGTH_SEC = 10
+# CORE_PATH = "/home/ofer/clones/drones_gen"
+# VID_LENGTH_SEC = 10
 
-core_utils.safe_rclpy_init()
+# core_utils.safe_rclpy_init()
 
 # with HostIsaacManager(core_path=CORE_PATH, com_udp=True, show_isaac_logs=False, bbox_publisher=False):
 
@@ -43,40 +43,60 @@ core_utils.safe_rclpy_init()
 
 
 
-tff = 2.0
+time_for_fifty = 2.0
 
 
-camera_point_sender = OnePointSender(lat=32.19965, lon=35.30593, alt=1000.0,
-                            roll=0.0, pitch=0.0, yaw=0.0)
+# camera_point_sender = OnePointSender(lat=32.19965, lon=35.30593, alt=1000.0,
+#                             roll=0.0, pitch=0.0, yaw=0.0)
 
-drone_bot = UdpBot(start_lat=32.19965, start_lon=35.30593, start_alt=1000.0,
+drone_bot = UdpBot(start_lat=32.20647, start_lon=35.29034, start_alt=540.0,
                     start_roll_d=0.0, start_pitch_d=0.0, start_yaw_d=0.0,
                     udp_port=33335, send_rate_hz=30.0)
 
-camera_point_sender.run(blocking=False)
+# camera_point_sender.run(blocking=False)
 drone_bot.run(blocking=False)
 
-drone_bot.move_forward_backward(distance_m=200.0, duration_s=tff)
+def drone_to_start():
 
-for i in range(3):
+    drone_bot.move_right_left(distance_m=200.0, duration_s=time_for_fifty)
+    drone_bot.move_forward_backward(distance_m=50.0, duration_s=time_for_fifty)
+    drone_bot.move_up_down(distance_m=50.0, duration_s=time_for_fifty)
 
-    drone_bot.move_forward_backward(distance_m=100, duration_s=tff)
+def scene_1():
 
-    drone_bot.move_right_left(distance_m=50.0, duration_s=tff)
-    drone_bot.move_up_down(distance_m=50.0, duration_s=tff)
+    drone_to_start()
+    drone_bot.move_forward_backward(distance_m=-400.0, duration_s=(time_for_fifty * 4))
 
-    drone_bot.move_right_left(distance_m=-100.0, duration_s=(tff * 2))
-    drone_bot.move_up_down(distance_m=-100.0, duration_s=(tff * 2))
+def scene_2():
+    
+    drone_to_start()
+    drone_bot.move_forward_backward(distance_m=-150.0, duration_s=time_for_fifty)
+    drone_bot.move_to_point(target_lat=32.20647, target_lon=35.29034, target_alt=530.0,
+                            target_yaw_d=0.0, target_roll_d=0.0, target_pitch_d=0.0,
+                            duration_s=(time_for_fifty * 4))
 
-    drone_bot.move_right_left(distance_m=100.0, duration_s=(tff * 2))
-    drone_bot.move_up_down(distance_m=50.0, duration_s=tff)
+scene_2()
+    
 
-    drone_bot.move_right_left(distance_m=-50.0, duration_s=tff)
+# for i in range(3):
+
+#     drone_bot.move_forward_backward(distance_m=100, duration_s=time_for_fifty)
+
+#     drone_bot.move_right_left(distance_m=50.0, duration_s=time_for_fifty)
+#     drone_bot.move_up_down(distance_m=50.0, duration_s=time_for_fifty)
+
+#     drone_bot.move_right_left(distance_m=-100.0, duration_s=(time_for_fifty * 2))
+#     drone_bot.move_up_down(distance_m=-100.0, duration_s=(time_for_fifty * 2))
+
+#     drone_bot.move_right_left(distance_m=100.0, duration_s=(time_for_fifty * 2))
+#     drone_bot.move_up_down(distance_m=50.0, duration_s=time_for_fifty)
+
+#     drone_bot.move_right_left(distance_m=-50.0, duration_s=time_for_fifty)
 
 
 
-camera_point_sender.close()
-drone_bot.close()
+# camera_point_sender.close()
+# drone_bot.close()
 
 
 # drone_bot.move_forward_backward(distance_m=30.0, duration_s=3.0)
